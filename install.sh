8652ea72ea49eabca3d951a640ec10f357df4d91
tjt263 <tjt263@gmail.com>
2025-06-14 22:08:09 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euo pipefail
+
+TMP_DIR="$(mktemp -d)"
+cd "$TMP_DIR"
+
+echo "[*] Downloading pystrap..."
+curl -sSL https://raw.githubusercontent.com/YOURNAME/bootstrappy/main/pystrap.sh -o pystrap
+chmod +x pystrap
+
+echo "[*] Installing to /usr/local/bin/pystrap..."
+sudo mv pystrap /usr/local/bin/pystrap
+
+echo "[+] Done. You can now run: pystrap"
+rm -rf "$TMP_DIR"
