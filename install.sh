ff7c8afe5293b59f14dc0000d4e6907dd44666b3
tjt263 <tjt263@gmail.com>
2025-06-14 22:45:29 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ TMP_DIR="$(mktemp -d)"
 cd "$TMP_DIR"
 
 echo "[*] Downloading pystrap..."
-curl -sSL https://raw.githubusercontent.com/YOURNAME/bootstrappy/main/pystrap.sh -o pystrap
+curl -sSL https://raw.githubusercontent.com/tjt263/pystrap/main/pystrap.sh -o pystrap
 chmod +x pystrap
 
 echo "[*] Installing to /usr/local/bin/pystrap..."
